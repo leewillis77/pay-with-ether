6a46cd97fe6caa821841ce6e7e390eb30ccba5b8
Lee Willis <leewillis77@gmail.com>
2017-03-15 19:29:19 +0000

    Exclude assets

--- deploy.sh
+++ deploy.sh
@@ -64,7 +64,8 @@ echo "Ignoring github specific files and deployment script"
 svn propset svn:ignore "deploy.sh
 README.md
 .git
-.gitignore" "$SVNPATH/trunk/"
+.gitignore
+assets" "$SVNPATH/trunk/"
 
 echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk/
