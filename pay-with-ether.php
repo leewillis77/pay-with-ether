ddda994d0477c4de330d17ffce440b2ee0a1e6b5
Lee Willis <lee@ademti-software.co.uk>
2017-12-07 10:44:39 +0000

    Bump (v1.3.1)

--- pay-with-ether.php
+++ pay-with-ether.php
@@ -4,9 +4,9 @@
 Plugin Name: Pay With Ether
 Plugin URI: https://wordpress.org/pay-with-ether
 Description: Payment gateway for accepting payments using Ether. Offers integration with PayWithEther.com for automated payment processing.
-Version: 1.3.0
+Version: 1.3.1
 WC requires at least: 2.6.0
-WC tested up to: 3.2.0
+WC tested up to: 3.3.0
 Author: Lee Willis
 Author URI: http://plugins.leewillis.co.uk/
 
