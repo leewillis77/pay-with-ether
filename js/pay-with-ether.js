a27be8895a42032b59f56b9777e297009d93b49d
Lee Willis <lee@ademti-software.co.uk>
2021-04-06 13:34:14 +0100

    Initial changes, still toWei() to replace

--- js/pay-with-ether.js
+++ js/pay-with-ether.js
@@ -1,15 +1,22 @@
 function pwe_sendTransaction() {
-	web3.eth.sendTransaction(
+	const txData = [
 		{
 			to: window.pwe.payment_address,
-			value: web3.toWei(window.pwe.eth_value, 'ether'),
+			value: web3.toWei( window.pwe.eth_value, 'ether' ),
 			data: window.pwe.tx_ref,
-		}, function(e, res) {}
+		}
+	];
+	window.ethereum.request(
+		{
+			method: 'eth_sendTransaction',
+			params: txData,
+		}
 	);
 }
-window.addEventListener('load', function() {
-	if (typeof web3 !== 'undefined') {
-		jQuery('.pwe-metamask-button').show();
-		jQuery('.pwe-metamask-button').on('click', pwe_sendTransaction);
+
+window.addEventListener( 'load', function () {
+	if ( typeof window.ethereum !== 'undefined' ) {
+		jQuery( '.pwe-metamask-button' ).show();
+		jQuery( '.pwe-metamask-button' ).on( 'click', pwe_sendTransaction );
 	}
-});
+} );
